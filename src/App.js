c91498244955facd4a9838915b203f87e4b8b551
n00bNn00b <reldewan9@gmail.com>
2022-04-05 18:11:28 +0600

    mock api created for the review app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Blogs from "./components/Blogs/Blogs";
+import Dashboard from "./components/Dashboard/Dashboard";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,22 @@
 import React from "react";
-import About from "../About/About";
-import Blogs from "../Blogs/Blogs";
-import Dashboard from "../Dashboard/Dashboard";
-import Home from "../Home/Home";
-import Reviews from "../Reviews/Reviews";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div>
-      <h2>Header working or not?</h2>
-      <Home />
-      <Reviews />
-      <Dashboard />
-      <Blogs />
-      <About />
+      <nav className="nav-bar">
+        <div>
+          <h2>Guitar Smith</h2>
+        </div>
+        <div className="menu-item">
+          <Link to="/home">Home</Link>
+          <Link to="/reviews">Reviews</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/blogs">Blogs</Link>
+          <Link to="/about">About Us</Link>
+        </div>
+      </nav>
     </div>
   );
 };
